bf6b8a67c1f6ae1654ef4cc1e5851aba8d62b0a5
Lorenzo <lorenzogarcil@gmail.com>
2017-03-17 12:30:57 +0100

    lm2

---
+++ LMA/Tarea 9 - Jquery/js/js.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    function() {
+                $("h1").addClass("h1")
+                $("#schedule caption").addClass("schedule")
+                $("#marks tr:odd").addClass("marksodd")
+                $("#marks tr:even").addClass("markseven")
+                $("p").hide()
+                $(".show").show()
+              }
+     
+    );
